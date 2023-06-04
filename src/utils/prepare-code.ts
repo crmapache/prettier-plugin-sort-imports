679073eb0e3e92eddd5f67fd73bacc738bc052e2
Maksim Zolotoi <crmapache@icloud.com>
2023-06-03 22:02:51 -0600

    delete logs

--- src/utils/prepare-code.ts
+++ src/utils/prepare-code.ts
@@ -2,19 +2,19 @@ import { ImportGroups } from '../types'
 
 export const prepareCode = (importGroups: ImportGroups) => {
   let result = ''
-  console.log('importGroups.libraries >>', importGroups.libraries)
+
   for (const importData of importGroups.libraries) {
     result += `${importData.raw}\n`
   }
 
   result += '\n'
-  console.log('importGroups.aliases >>', importGroups.aliases)
+
   for (const importData of importGroups.aliases) {
     result += `${importData.raw}\n`
   }
 
   result += '\n'
-  console.log('importGroups.relatives >>', importGroups.relatives)
+
   for (const importData of importGroups.relatives) {
     result += `${importData.raw}\n`
   }
